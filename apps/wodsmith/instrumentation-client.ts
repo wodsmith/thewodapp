dcfaeaff3920b4bd267449f34af4d548503b9fad
Ian Jones <jones58ian@gmail.com>
2025-12-17 22:04:43 -0700

    Fix posthog

--- apps/wodsmith/instrumentation-client.ts
+++ apps/wodsmith/instrumentation-client.ts
@@ -5,7 +5,7 @@ const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY
 
 if (isProd && posthogKey) {
 	posthog.init(posthogKey, {
-		api_host: "/ingest",
+		api_host: "https://analytics.wodsmith.com",
 		ui_host: "https://us.posthog.com",
 		defaults: "2025-05-24",
 		capture_exceptions: true,
